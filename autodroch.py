a3b5f38859e9fe93515a606f4f7832e5e8fa30b7
markfisher101 <142403252+markfisher101@users.noreply.github.com>
2023-08-16 23:38:29 +0500

    Update autodroch.py

--- autodroch.py
+++ autodroch.py
@@ -13,41 +13,37 @@ from .. import loader, utils
 
 @loader.tds
 class AutoDrochMod(loader.Module):
-    """Модуль для автоматической дрочки и выращивания хуя в боте @xyu_epta_bot"""
+    """Модуль для автоматической дрочки и выращивания хуя в боте @xyu_epta_bot
+    Использовать команду старта в чате где нужно фармить."""
 
     strings = {"name": "AutoDroch"}
 
     def __init__(self):
         self.tasks = []
 
-    async def a_run(self, client):
+    async def a_run(self, message):
         while True:
-            await message.send_message("/drochnut@hyu_epta_bot")
-            await asyncio.sleep(3900)
-
-    async def b_run(self, client):
-        while True:
-            await message.send_message("/dick@hyu_epta_bot")
+            await message.reply("/dick@xyu_epta_bot")
             await asyncio.sleep(21600)
-
+            await message.reply("/drochnut@xyu_epta_bot")
+            await asyncio.sleep(3960)
 
     @loader.unrestricted
     @loader.ratelimit
     async def drochcmd(self, message):
-        """Запустить автоматическую дрочку"""
+        """Запустить автодрочку в боте"""
         if self.tasks:
-            return await message.edit("Авто дрочка уже запущена.")
-        await message.edit("Авто дрочка запущена.")
-        client = message.client
-        self.tasks = [asyncio.create_task(self.a_run(message)), asyncio.create_task(self.b_run(message))]
+            return await message.edit("Автодрочка уже запущена.")
+        await message.edit("Автодрочка запущена.")
+        self.tasks = [asyncio.create_task(self.a_run(message)),]
 
     @loader.unrestricted
     @loader.ratelimit
-    async def drochoffcmd(self, message):
-        """Остановить автоматическую дрочку"""
+    async def drochstopcmd(self, message):
+        """Остановить автодрочкуг в боте"""
         if not self.tasks:
-            return await message.edit("Авто дрочка не запущена.")
+            return await message.edit("Автодрочка не запущена.")
         for task in self.tasks:
             task.cancel()
         self.tasks = []
-        await message.edit("Авто дрочка остановлена.")
+        await message.edit("Автодрочка остановлена.")
