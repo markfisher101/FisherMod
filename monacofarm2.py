210263c13c3345ee89230dd8bc31bc904f8c987b
markfisher101 <142403252+markfisher101@users.noreply.github.com>
2023-09-01 13:22:29 +0500

    Update monacofarm2.py

--- monacofarm2.py
+++ monacofarm2.py
@@ -31,27 +31,33 @@ class FarmMonacoMod(loader.Module):
             await client.send_message('@monacogamebot', "Бизнес снять")
             await asyncio.sleep(3605)
 
-    async def l_run(self, client):
+    async def с_run(self, client):
         while True:
             await asyncio.sleep(10)
+            await client.send_message('@monacogamebot', "Собирать листья")
+            await asyncio.sleep(605)
+
+    async def l_run(self, client):
+        while True:
+            await asyncio.sleep(15)
             await client.send_message('@monacogamebot', "Город снять")
             await asyncio.sleep(3605)
 
     async def t_run(self, client):
         while True:
-            await asyncio.sleep(15)
+            await asyncio.sleep(20)
             await client.send_message('@monacogamebot', "Работать")
             await asyncio.sleep(1805)
 
     async def a_run(self, client):
         while True:
-            await asyncio.sleep(20)
+            await asyncio.sleep(25)
             await client.send_message('@monacogamebot', "Ферма снять")
             await asyncio.sleep(3605)
 
     async def q_run(self, client):
         while True:
-            await asyncio.sleep(25)
+            await asyncio.sleep(30)
             await client.send_message('@monacogamebot', "Ограбить")
             await asyncio.sleep(43205)
 
@@ -63,7 +69,7 @@ class FarmMonacoMod(loader.Module):
             return await message.edit("Автоматический фарминг уже запущен.")
         await message.edit("Автоматический фарминг запущен.")
         client = message.client
-        self.tasks = [asyncio.create_task(self.b_run(client)), asyncio.create_task(self.p_run(client)), asyncio.create_task(self.l_run(client)), asyncio.create_task(self.t_run(client)), asyncio.create_task(self.a_run(client)), asyncio.create_task(self.q_run(client))]
+        self.tasks = [asyncio.create_task(self.b_run(client)), asyncio.create_task(self.c_run(client)), asyncio.create_task(self.p_run(client)), asyncio.create_task(self.l_run(client)), asyncio.create_task(self.t_run(client)), asyncio.create_task(self.a_run(client)), asyncio.create_task(self.q_run(client))]
 
     @loader.unrestricted
     @loader.ratelimit
